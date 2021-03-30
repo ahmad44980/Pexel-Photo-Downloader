313bad15dbbfb38c5ca2a31986e24fefa1255530
Ahmad Mahmood <ahmadrizvi9906@gmail.com>
2021-03-30 19:28:46 +0500

    re-factor

---
+++ pexels/helper/helper.js
@@ -0,0 +1,35 @@
+const { DownloaderHelper } = require('node-downloader-helper');
+const fetch = require("node-fetch");
+var fs = require('fs');
+
+var dir = './result';
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+function formatBytes(a, b = 2) {
+    if (0 === a) return "0 Bytes";
+    const c = 0 > b ? 0 : b,
+        d = Math.floor(Math.log(a) / Math.log(1024));
+    return parseFloat((a / Math.pow(1024, d)).toFixed(c)) + " " + ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][d]
+}
+
+module.exports.getPhotos = function(images) {
+    var k = 0;
+    images.map(image => {
+        var url = image.src.medium;
+
+        const dl = new DownloaderHelper(url, dir);
+        k = k + 1;
+
+        dl
+            .on('download', downloadInfo => console.log('Download Begins: ', {
+                name: downloadInfo.fileName,
+                size: formatBytes(downloadInfo.totalSize)
+            }))
+
+        dl.start();
+
+    })
+    console.log('Total Files Downloaded: ', k);
+}
--- pexels/index.js
+++ pexels/index.js
@@ -1,28 +1,13 @@
-const { DownloaderHelper } = require('node-downloader-helper');
 const fetch = require("node-fetch");
-var fs = require('fs');
-const pkg = require('../package.json');
 const secrets = require('./secrets');
+const helper = require('./helper/helper');
 
 var i = 0;
-var k = 0;
-var dir = './result';
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-}
 
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-function formatBytes(a, b = 2) {
-    if (0 === a) return "0 Bytes";
-    const c = 0 > b ? 0 : b,
-        d = Math.floor(Math.log(a) / Math.log(1024));
-    return parseFloat((a / Math.pow(1024, d)).toFixed(c)) + " " + ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][d]
-}
-
 
 readline.question('Search Photos: ', search => {
     readline.question('Total Photos?  ', total => {
@@ -37,29 +22,10 @@ readline.question('Search Photos: ', search => {
                     return resp.json()
                 })
                 .then(data => {
-                    getPhotos(data.photos);
+                    helper.getPhotos(data.photos);
                     console.log('Results Found:',
                         data.photos.length, 'on Page', i);
                 });
-
-            async function getPhotos(images) {
-                images.map(image => {
-                    var url = image.src.medium;
-
-                    const dl = new DownloaderHelper(url, dir);
-                    k = k + 1;
-
-                    dl
-                        .on('download', downloadInfo => console.log('Download Begins: ', {
-                            name: downloadInfo.fileName,
-                            size: formatBytes(downloadInfo.totalSize)
-                        }))
-
-                    dl.start();
-
-                })
-                console.log('Total Files Downloaded: ', k);
-            }
 
             readline.close();
         } else {
